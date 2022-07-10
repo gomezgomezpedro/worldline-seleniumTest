fb7c71d775ee7629a0e9d3a23d59b0ccead449ed
Pedro Gómez Gómez <pgomezcuadrado@gmail.com>
2022-07-10 23:02:46 +0200

    Added Logging tool and log messages alongside the test cases

--- src/test/java/StepDefinitions/GoogleSteps.java
+++ src/test/java/StepDefinitions/GoogleSteps.java
@@ -3,21 +3,26 @@ import Pages.GoogleMainPage;
 import Pages.GoogleResultsPage;
 import UtilityClasses.WebDriverManager;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.After;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GoogleSteps {
     WebDriver driver;
     WebDriverManager manager = new WebDriverManager();
     GoogleMainPage mainPage;
     GoogleResultsPage resultsPage;
+    Logger logger = LoggerFactory.getLogger(GoogleSteps.class);
 
     //This method initializes the driver and all the website pages required in the test case
     @Before
-    public void setup(){
+    public void setup(Scenario scenario){
+        logger.info("Scenario: " + scenario.getName());
         driver = manager.startDriver();
         mainPage = new GoogleMainPage(driver);
         resultsPage = new GoogleResultsPage(driver);
@@ -25,6 +30,7 @@ public class GoogleSteps {
 
     @When("the user inputs {string} as a search value")
     public void the_user_inputs_as_a_search_value(String searchValue) throws InterruptedException {
+        logger.info("When the user inputs {} as a search value", searchValue);
         mainPage.acceptTerms();
         Thread.sleep(2000);
         mainPage.inputSearchValue(searchValue);
@@ -33,15 +39,21 @@ public class GoogleSteps {
 
     @When("the user clicks the Search button")
     public void the_user_clicks_the_search_button() throws InterruptedException {
+        logger.info("And the user clicks the Search button");
         mainPage.clickSearchButton();
         Thread.sleep(2000);
     }
 
     @Then("at least one result is displayed")
     public void at_least_one_result_is_displayed() {
+        logger.info("Then at least one result is displayed");
         Assert.assertTrue(resultsPage.isFirstResultDisplayed());
     }
 
+    @After
+    public void logTestResult(Scenario scenario){
+        logger.info("TEST RESULT: " + scenario.getStatus().toString());
+    }
     @After
     public void tearDown(){
         manager.quitDriver(driver);
--- src/test/java/UtilityClasses/WebDriverManager.java
+++ src/test/java/UtilityClasses/WebDriverManager.java
@@ -1,12 +1,16 @@
 package UtilityClasses;
+import StepDefinitions.GoogleSteps;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverManager {
+
+    Logger logger = LoggerFactory.getLogger(WebDriverManager.class);
 
     public WebDriver startDriver (){
         WebDriver driver;
@@ -14,6 +18,7 @@ public class WebDriverManager {
         System.setProperty("webdriver.chrome.driver", props.getProperty("webdriverPath"));
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
+        logger.debug("Initializing Google Chrome browser and navigating to {} ", props.getProperty("websiteUrl"));
         driver = new ChromeDriver(options);
         driver.get(props.getProperty("websiteUrl"));
 
@@ -21,6 +26,7 @@ public class WebDriverManager {
     }
 
     public void quitDriver (WebDriver driver) {
+        logger.debug("Closing browser and chromedriver task");
         driver.quit();
     }
 }
